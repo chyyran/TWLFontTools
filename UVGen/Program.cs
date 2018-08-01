26b1ffef1f651b871e89d98f3871e9ac8b84426c
RonnChyran <ronny6993@gmail.com>
2018-08-01 11:04:19 -0400

    start work on uvgen

---
+++ UVGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UVGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UVGen/UVGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UVGen
+{
+    public class UVGenerator
+    {
+        public static readonly int CharactersPerLine = 0x20;
+    }
+}
